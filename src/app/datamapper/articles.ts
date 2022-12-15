0e8f322fc372d11f54ec7fc6f165cd94695838b1
GaetanSantucci <gaetan.santucci@outlook.com>
2022-12-15 21:35:00 +0100

    :boom: Add new feature Types with first TS Interface

--- src/app/datamapper/articles.ts
+++ src/app/datamapper/articles.ts
@@ -1,13 +1,14 @@
 import { pool } from '../service/dbClient.js';
 import debug from 'debug';
 const logger = debug('Datamapper')
+import { Article } from '../Types/articles.js'
 
 const getArticles = async () => {
   const result = await pool.query('SELECT * FROM article')
   return result.rows
 }
 
-const getArticle: (id: number) => Promise<{ id: number, title: string, description: string }> = async (id: number) => {
+const getArticle: (id: number) => Promise<Article> = async (id) => {
   const query = `SELECT * FROM article WHERE id = $1;`
   const result = await pool.query(query, [id])
   logger('result: ', result.rows);
